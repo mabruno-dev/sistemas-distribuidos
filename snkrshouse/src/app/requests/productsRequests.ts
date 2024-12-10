29fc5d9a54b5e63c9657fab7177f8f5d926c79f8
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2024-12-10 18:41:24 -0300

    change the target port of the api

--- snkrshouse/src/app/requests/productsRequests.ts
+++ snkrshouse/src/app/requests/productsRequests.ts
@@ -1,32 +1,30 @@
-import axios from "axios";
-import {PostProduct} from '@/app/types/product'
+import axios from 'axios';
+import { PostProduct } from '@/app/types/product';
 
-const URL = "http://localhost:5000/public/product";
+const URL = 'http://localhost:8000/public/product';
 
 const headers = {
-    headers: {
-        accept: "application/json",
-    },
+	headers: {
+		accept: 'application/json',
+	},
 };
 
 export async function getProducts() {
-    const response = await axios.get(URL, headers);
-    const data = response.data;
-    return data;
+	const response = await axios.get(URL, headers);
+	const data = response.data;
+	return data;
 }
 
 export async function postProduct(product: PostProduct) {
-    const response = await axios.post(URL, product, headers);
-    return response;
+	const response = await axios.post(URL, product, headers);
+	return response;
 }
 
-export async function deleteProduct(id:number) {
-    
-    const response = await axios.delete(`${URL}/?product_id=${id}`, headers);
-    return response;
-    
+export async function deleteProduct(id: number) {
+	const response = await axios.delete(`${URL}/?product_id=${id}`, headers);
+	return response;
 }
 
-// export async function updateProduct(id, product) {  
-//     
+// export async function updateProduct(id, product) {
+//
 // }
